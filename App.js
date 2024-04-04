93cea3df9bf60969c095e90dd3c69a0ad31f1b6e
qich3n <chen.qi3@northeastern.edu>
2024-04-03 21:18:56 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import Lab5 from './lab5.js';
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+
+const app = express()
+app.use(cors())
+app.use(express.json()) // statement occurs AFTER setting up CORS
+CourseRoutes(app);
+Lab5(app);
+ModuleRoutes(app);
+app.listen(process.env.PORT || 4000)
