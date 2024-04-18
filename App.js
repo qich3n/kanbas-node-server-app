f9d974bcbd4ac13b9b916328ff979ea5517c86e0
qich3n <chen.qi3@northeastern.edu>
2024-04-18 14:39:39 -0400

    ohh

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
   })
  ); 
  const sessionOptions = {
